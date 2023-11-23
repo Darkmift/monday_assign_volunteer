4a1ed6a78ddc57b3075e4eb516cf1e461a7ccd9f
aek <darkmift@gmail.com>
2023-11-23 17:18:20 -0300

    [main] types changes

--- monday_assign_volunteer/types/index.ts
+++ monday_assign_volunteer/types/index.ts
@@ -1,11 +1,21 @@
 export interface IColumnValue {
     id: string;
-    value: string; // Keeping it as string since the value is JSON encoded
+    value: string | null; // Keeping it as string since the value is JSON encoded
 }
 
 export interface IGroup {
     id: string;
     title: string;
+}
+
+/**
+ * This is the data structure for מבקשי סיוע column in IItem
+ */
+export interface IBoardRelation {
+    id: 'board_relation';
+    data: {
+        linkedPulseIds: { linkedPulseId: number }[];
+    };
 }
 
 export interface IItem {
@@ -15,6 +25,25 @@ export interface IItem {
     group: IGroup;
 }
 
-export interface IVolunteer extends Omit<IItem, 'column_values'> {
+export interface ILanguageDataColumnValue {
+    id: string;
+    checked: boolean;
+}
+
+/**
+ * Extends user data with the language fields
+ */
+export interface Ientity extends IItem {
+    languages: {
+        [key: string]: ILanguageDataColumnValue;
+    };
+}
+
+export interface IVolunteer extends Ientity {
+    [key: string]: unknown;
+    capacity: { id: 'numbers'; data?: number | null };
+}
+
+export interface IHelpRequester extends Ientity {
     [key: string]: unknown;
 }
