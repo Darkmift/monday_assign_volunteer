aef6e5420a4a3e151ef7f9121d49996e79ac6243
aek <darkmift@gmail.com>
2023-11-23 23:51:53 -0300

    [main] Refactor tryParse function to accept unknown input

--- monday_assign_volunteer/utils/tryparse.ts
+++ monday_assign_volunteer/utils/tryparse.ts
@@ -1,10 +1,10 @@
-const tryParse = (input: string | null): object | string | null => {
+const tryParse = (input: unknown): unknown => {
     if (!input) {
         return input;
     }
     try {
         // Attempt to parse the input as JSON
-        return JSON.parse(input);
+        return JSON.parse(input as string);
     } catch (error) {
         // If parsing fails, return the original input
         return input;
