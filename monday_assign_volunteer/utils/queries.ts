f2060400e30c439e2b8d9a81c545aa72e36c98dd
aek <darkmift@gmail.com>
2023-11-26 03:25:33 -0300

    [V2] optimized queries

--- monday_assign_volunteer/utils/queries.ts
+++ monday_assign_volunteer/utils/queries.ts
@@ -43,16 +43,37 @@ export const GET_VOLUNTEER_BOARD = gql`
     }
 `;
 
-export const GET_HELP_REQUESTER_DATA = gql`
-    query GetHelpRequesterData($helpRequesterId: ID!, $helpRequesterBoardId: ID!) {
-        boards(ids: [$helpRequesterBoardId]) {
+export const GET_ALL_ACTIVE_VOLUNTEERS = gql`
+    query GetAllActiveVolunteers($boardId: ID!) {
+        boards(ids: [$boardId]) {
             name
             id
             columns {
                 id
                 title
+                type
+            }
+            groups(ids: ["topics"]) {
+                id
+                title
+                items_page {
+                    items {
+                        id
+                        name
+                        column_values(ids: ["numbers", "board_relation", "phone", "checkbox", "checkbox4"]) {
+                            type
+                            id
+                            value
+                        }
+                    }
+                }
             }
         }
+    }
+`;
+
+export const GET_HELP_REQUESTER_DATA = gql`
+    query GetHelpRequesterData($helpRequesterId: ID!) {
         items(ids: [$helpRequesterId]) {
             id
             name
@@ -60,9 +81,17 @@ export const GET_HELP_REQUESTER_DATA = gql`
                 id
                 title
             }
+            board {
+                columns {
+                    id
+                    title
+                    type
+                }
+            }
             column_values {
                 id
                 value
+                type
             }
             updated_at
         }
@@ -70,8 +99,11 @@ export const GET_HELP_REQUESTER_DATA = gql`
 `;
 
 export const UPDATE_COLUMN_VALUE_FOR_ITEM_IN_BOARD = gql`
-    mutation ChangeColumnValue($volunteerId: ID!, $boardId: ID!, $columnId: String!, $value: JSON!) {
-        change_column_value(item_id: $volunteerId, board_id: $boardId, column_id: $columnId, value: $value) {
+    mutation ChangeColumnValue($helpRequesterId: ID!, $boardId: ID!, $columnId: String!, $value: JSON!) {
+        change_column_value(item_id: $helpRequesterId, board_id: $boardId, column_id: $columnId, value: $value) {
+            id
+        }
+        move_item_to_group(group_id: "new_group73364", item_id: $helpRequesterId) {
             id
         }
     }
