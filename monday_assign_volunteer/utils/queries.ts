d291960702a387bb638a73cf205af3a200b81c19
aek <darkmift@gmail.com>
2023-11-30 00:56:40 -0300

    [on-no-assign-move-back-group] Add queries for getting volunteer columns and volunteers grouped by language

--- monday_assign_volunteer/utils/queries.ts
+++ monday_assign_volunteer/utils/queries.ts
@@ -16,6 +16,20 @@ export const GET_ITEMS_QUERY = gql`
     }
 `;
 
+export const GET_VOLUNTEER_COLUMNS = gql`
+    query GetVolunteerColumns($boardId: ID!) {
+        boards(ids: [$boardId]) {
+            name
+            id
+            columns {
+                id
+                title
+                type
+            }
+        }
+    }
+`;
+
 export const GET_VOLUNTEER_BOARD = gql`
     query GetVolunteerBoard($boardId: ID!) {
         boards(ids: [$boardId]) {
@@ -99,12 +113,57 @@ export const GET_HELP_REQUESTER_DATA = gql`
 `;
 
 export const UPDATE_COLUMN_VALUE_FOR_ITEM_IN_BOARD = gql`
-    mutation ChangeColumnValue($helpRequesterId: ID!, $boardId: ID!, $columnId: String!, $value: JSON!) {
+    mutation ChangeColumnValue(
+        $groupId: String!
+        $helpRequesterId: ID!
+        $boardId: ID!
+        $columnId: String!
+        $value: JSON!
+    ) {
         change_column_value(item_id: $helpRequesterId, board_id: $boardId, column_id: $columnId, value: $value) {
             id
         }
-        move_item_to_group(group_id: "new_group73364", item_id: $helpRequesterId) {
+        move_item_to_group(group_id: $groupId, item_id: $helpRequesterId) {
             id
+        }
+    }
+`;
+
+export const MOVE_HELPREQUESTER_BACK_TO_RAWLIST_GROUP = gql`
+    mutation ChangeColumnValue($helpRequesterId: ID!, $groupId: String!) {
+        move_item_to_group(group_id: $groupId, item_id: $helpRequesterId) {
+            id
+        }
+    }
+`;
+
+export const GET_VOLUNTEERS_GROUPED_BY_LANGUAGE = gql`
+    query GetVolunteersGroupedByCanSpeakLanguage(
+        $boardId: ID!
+        $groupId: String!
+        $langColId: ID!
+        $capacityColId1: String!
+        $capacityColId2: ID!
+        $limit: Int
+    ) {
+        boards(ids: [$boardId]) {
+            groups(ids: [$groupId]) {
+                items_page(
+                    limit: $limit
+                    query_params: {
+                        rules: [
+                            { column_id: $langColId, compare_value: [null], operator: is_not_empty }
+                            { column_id: $capacityColId2, compare_value: [8], operator: lower_than }
+                        ]
+                        order_by: [{ column_id: $capacityColId1, direction: asc }]
+                    }
+                ) {
+                    items {
+                        id
+                        name
+                    }
+                }
+            }
         }
     }
 `;
