c1ff04d5069abd3462b0d40ec385178daa4916de
aek <darkmift@gmail.com>
2023-11-30 01:31:18 -0300

    [on-no-assign-move-back-group] Refactor makeGQLRequest function signature

--- monday_assign_volunteer/utils/graphQlRequestClient.ts
+++ monday_assign_volunteer/utils/graphQlRequestClient.ts
@@ -1,7 +1,7 @@
 import request from 'graphql-request';
 import { MONDAY_API_KEY, MONDAY_API_URL } from '../config/consts';
 
-const makeGQLRequest = async (query: string, variables: Record<string, unknown>): Promise<unknown> => {
+const makeGQLRequest = async <T = unknown>(query: string, variables: Record<string, unknown>): Promise<T> => {
     return await request(MONDAY_API_URL, query, variables, {
         'Content-Type': 'application/json',
         Authorization: MONDAY_API_KEY,
